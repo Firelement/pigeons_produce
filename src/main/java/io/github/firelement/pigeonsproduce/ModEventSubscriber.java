b20e58d9c0256463abc3a23acd0a6ff29ea2926c
PrintsCharmingJewelry Josh <snickers07@comcast.net>
2020-01-23 20:04:05 -0700

    created a food property

--- src/main/java/io/github/firelement/pigeonsproduce/ModEventSubscriber.java
+++ src/main/java/io/github/firelement/pigeonsproduce/ModEventSubscriber.java
@@ -2,6 +2,7 @@ package io.github.firelement.pigeonsproduce;
 
 
 import io.github.firelement.pigeonsproduce.init.ModItemGroups;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
@@ -12,11 +13,12 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 @EventBusSubscriber(modid = PigeonsProduce.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEventSubscriber {
 
+
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event){
         event.getRegistry().registerAll(
-                setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "example_item"),
-                setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "cheese")
+                setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).food(new Food.Builder().hunger(1).saturation(0.6F).build())), "example_item"),
+                setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).food(new Food.Builder().hunger(3).saturation(0.6F).build())), "cheese")
         );
     }
 

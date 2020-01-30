874fd2a4b596cef119ad6b874ef79d689d901f93
PrintsCharmingJewelry Josh <snickers07@comcast.net>
2020-01-30 09:59:07 -0700

    Added In Class for custom plant and implementing vanilla minecraft coding to try to make plant work
    Error with naming I assume, More debugging needed

--- src/main/java/io/github/firelement/pigeonsproduce/init/ModBlocks.java
+++ src/main/java/io/github/firelement/pigeonsproduce/init/ModBlocks.java
@@ -2,8 +2,12 @@ package io.github.firelement.pigeonsproduce.init;
 
 import io.github.firelement.pigeonsproduce.PigeonsProduce;
 import io.github.firelement.pigeonsproduce.block.ModFurnaceBlock;
+import io.github.firelement.pigeonsproduce.block.TomatoPlant;
 import net.minecraft.block.Block;
+import net.minecraft.block.CropsBlock;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.registry.Registry;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,6 +16,11 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, PigeonsProduce.MODID);
 
     public static final RegistryObject<Block> MOD_FURNACE = BLOCKS.register("mod_furnace", () -> new ModFurnaceBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.5f).lightValue(13)));
-    public static final RegistryObject<Block> TOMATO_PLANT = BLOCKS.register("tomato_plant", () -> new Block(Block.Properties.create(Material.ORGANIC)));
+    public static final RegistryObject<Block> TOMATO_PLANT = BLOCKS.register("tomato_plant", () -> new TomatoPlant("tomato_plant"));
+    public static final Block TOMATO_PLANT_BLOCK = register("tomato_block", new TomatoPlant("tomato_block"));
 
+    //Block.Properties.create(Material.PLANTS).doesNotBlockMovement().tickRandomly().sound(SoundType.CROP).hardnessAndResistance(0,0)
+    private static Block register(String key, Block p_222382_1_) {
+        return Registry.register(Registry.BLOCK, key, p_222382_1_);
+    }
 }
--- src/main/java/io/github/firelement/pigeonsproduce/init/ModItems.java
+++ src/main/java/io/github/firelement/pigeonsproduce/init/ModItems.java
@@ -1,8 +1,12 @@
 package io.github.firelement.pigeonsproduce.init;
 
 import io.github.firelement.pigeonsproduce.PigeonsProduce;
-import net.minecraft.item.Food;
-import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.item.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,9 +18,37 @@ public class ModItems {
 
     public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).food(new Food.Builder().hunger(1).saturation(0.6F).build())));
     public static final RegistryObject<Item> CHEESE = ITEMS.register("cheese", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).food(new Food.Builder().hunger(3).saturation(0.6F).build())));
+    public static final Item TOMATO = register("tomato", new BlockNamedItem(ModBlocks.TOMATO_PLANT_BLOCK, (new Item.Properties()).group(ItemGroup.MATERIALS)));
 
-        /*event.getRegistry().registerAll(
+    /*event.getRegistry().registerAll(
                 setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).food(new Food.Builder().hunger(1).saturation(0.6F).build())), "example_item"),
                 setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).food(new Food.Builder().hunger(3).saturation(0.6F).build())), "cheese")
         );*/
+        private static Item register(Block blockIn) {
+            return register(new BlockItem(blockIn, new Item.Properties()));
+        }
+
+    private static Item register(Block blockIn, ItemGroup itemGroupIn) {
+        return register(new BlockItem(blockIn, (new Item.Properties()).group(itemGroupIn)));
+    }
+
+    private static Item register(BlockItem blockItemIn) {
+        return register(blockItemIn.getBlock(), blockItemIn);
+    }
+
+    protected static Item register(Block blockIn, Item itemIn) {
+        return register(Registry.BLOCK.getKey(blockIn), itemIn);
+    }
+
+    private static Item register(String key, Item itemIn) {
+        return register(new ResourceLocation(key), itemIn);
+    }
+
+    private static Item register(ResourceLocation key, Item itemIn) {
+        if (itemIn instanceof BlockItem) {
+            ((BlockItem)itemIn).addToBlockToItemMap(Item.BLOCK_TO_ITEM, itemIn);
+        }
+
+        return Registry.register(Registry.ITEM, key, itemIn);
+    }
 }

757c5f86526b44399431b9bd64c2c2b855333301
PrintsCharmingJewelry Josh <snickers07@comcast.net>
2020-02-24 13:02:36 -0700

    added Cooked Hamburger patty

--- src/main/java/io/github/firelement/pigeonsproduce/init/ModItems.java
+++ src/main/java/io/github/firelement/pigeonsproduce/init/ModItems.java
@@ -27,6 +27,8 @@ public class ModItems {
     public static final RegistryObject<Item> APPLE_VINEGAR = ITEMS.register("apple_vinegar", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> KETCHUP = ITEMS.register("ketchup", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> HAMBURGER_PATTY = ITEMS.register("hamburger_patty", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> COOKED_HAMBURGER_PATTY = ITEMS.register("cooked_hamburger_patty", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
     public static final RegistryObject<Item> SALT = ITEMS.register("salt", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> PEPPER = ITEMS.register("pepper", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> DOUGH = ITEMS.register("dough", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
